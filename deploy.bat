73dd9ea8a22205833908dcc88def0eb0b37ee5c0
gingerBill <bill@gingerbill.org>
2019-02-17 13:09:42 +0000

    Test Deployment

--- deploy.bat
+++ deploy.bat
@@ -8,13 +8,13 @@ call hugo
 
 call git add .
 git commit -m %1
-git push origin master
+git push GitHub master
 
 pushd public
 call git add .
 
 git commit -m %1
-git push origin master
+git push GitHub master
 popd
 
 goto end
